70f7a9e94c18150d20e4b5f37db89d22c93b54be
Renan Augusto <zoogthegamer@gmail.com>
2025-09-13 20:34:11 -0300

    Commit da Lista Encadeada de Processos
    Esqueci dos commits atômicos.

---
+++ src/ListaDeProcessos.java
@@ -0,0 +1,41 @@
+public class ListaDeProcessos {
+    private Node head;
+
+    public ListaDeProcessos() {
+        this.head = null;
+    }
+
+    public void adicionar(Processo processo) {
+        Node node = new Node(processo);
+        if (head == null) {
+            node = head;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    public void remover(int id) {
+        if (head == null) {
+            System.out.println("A lista está vazia!");
+            return;
+        }
+        else if (head.processo.getId() == id) {
+            head = head.next;
+            System.out.println("Processo Removido. id: " + id);
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            if (temp.next.processo.getId() == id){
+                temp.next = temp.next.next;
+                System.out.println("Processo Removido. id: " + id);
+                return;
+            }
+        }
+        System.out.println("O Processo com id:" + id +" não foi encontrado e não pôde ser removido.");
+    }
+}
